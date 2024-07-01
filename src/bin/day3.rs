e4cb9ec6576e9b972083212688c3ea8a248d4556
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-01 10:50:17 +0200

    day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 fn main() {
 	let file = read_to_string("input/day3.txt").unwrap();
@@ -7,6 +7,9 @@ fn main() {
     let coords = part1(input);
     let part1 = coords.0.abs() + coords.1.abs();
     println!("Day 3 part 1: {}", part1);
+
+    let part2 = part2(input as i32);
+    println!("Day 3 part 2: {}", part2);
 }
 
 fn part1(input: i64) -> (i64, i64) {
@@ -52,6 +55,92 @@ fn part1(input: i64) -> (i64, i64) {
             if steps == input {
                 return curr;
             }
+        }
+        maxy = curr.1;
+    }
+}
+
+fn part2(input: i32) -> i32 {
+    let mut miny = 0;
+    let mut maxy = 0;
+    let mut minx = 0;
+    let mut maxx = 0;
+
+    let mut curr = (0,0);
+
+    let mut prevs = HashMap::new();
+    prevs.insert(curr, 1);
+    loop {
+        while curr.0 <= maxx {
+            curr = (curr.0 + 1, curr.1);
+            let neighs = [
+                (curr.0 - 1, curr.1),
+                (curr.0 + 1, curr.1),
+                (curr.0, curr.1 - 1),
+                (curr.0, curr.1 + 1),
+                (curr.0 - 1, curr.1 + 1),
+                (curr.0 + 1, curr.1 + 1),
+                (curr.0 - 1, curr.1 - 1),
+                (curr.0 + 1, curr.1 - 1),
+            ].iter().filter(|xy| prevs.contains_key(xy)).map(|xy| prevs[xy]).sum::<i32>();
+            if input < neighs {
+                return neighs;
+            }
+            prevs.insert(curr.clone(), neighs);
+        }
+        maxx = curr.0;
+        while curr.1 >= miny {
+            curr = (curr.0, curr.1 - 1);
+            let neighs = [
+                (curr.0 - 1, curr.1),
+                (curr.0 + 1, curr.1),
+                (curr.0, curr.1 - 1),
+                (curr.0, curr.1 + 1),
+                (curr.0 - 1, curr.1 + 1),
+                (curr.0 + 1, curr.1 + 1),
+                (curr.0 - 1, curr.1 - 1),
+                (curr.0 + 1, curr.1 - 1),
+            ].iter().filter(|xy| prevs.contains_key(xy)).map(|xy| prevs[xy]).sum::<i32>();
+            if input < neighs {
+                return neighs;
+            }
+            prevs.insert(curr.clone(), neighs);
+        }
+        miny = curr.1;
+        while curr.0 >= minx {
+            curr = (curr.0 - 1, curr.1);
+            let neighs = [
+                (curr.0 - 1, curr.1),
+                (curr.0 + 1, curr.1),
+                (curr.0, curr.1 - 1),
+                (curr.0, curr.1 + 1),
+                (curr.0 - 1, curr.1 + 1),
+                (curr.0 + 1, curr.1 + 1),
+                (curr.0 - 1, curr.1 - 1),
+                (curr.0 + 1, curr.1 - 1),
+            ].iter().filter(|xy| prevs.contains_key(xy)).map(|xy| prevs[xy]).sum::<i32>();
+            if input < neighs {
+                return neighs;
+            }
+            prevs.insert(curr.clone(), neighs);
+        }
+        minx = curr.0;
+        while curr.1 <= maxy {
+            curr = (curr.0, curr.1 + 1);
+            let neighs = [
+                (curr.0 - 1, curr.1),
+                (curr.0 + 1, curr.1),
+                (curr.0, curr.1 - 1),
+                (curr.0, curr.1 + 1),
+                (curr.0 - 1, curr.1 + 1),
+                (curr.0 + 1, curr.1 + 1),
+                (curr.0 - 1, curr.1 - 1),
+                (curr.0 + 1, curr.1 - 1),
+            ].iter().filter(|xy| prevs.contains_key(xy)).map(|xy| prevs[xy]).sum::<i32>();
+            if input < neighs {
+                return neighs;
+            }
+            prevs.insert(curr.clone(), neighs);
         }
         maxy = curr.1;
     }
