819a2bba6a3f51e353ceaffa4eb916bbae71c4c7
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-01 18:09:00 +0200

    day 10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -24,4 +24,27 @@ fn main() {
 
 	let part1 = list[0] * list[1];
 	println!("Day 10 part 1: {}", part1);
+
+	let mut list = (0..=255).into_iter().collect::<Vec<_>>();
+	let mut curr = 0;
+	let mut skip_size = 0;
+	let lengths = file.chars().map(|x| x as u32 as usize).chain([17, 31, 73, 47, 23].into_iter()).collect::<Vec<_>>();
+	for _ in 0..64 {
+		for &length in &lengths {
+			let mut vec = Vec::new();
+			for i in 0..length {
+				vec.push(list[(i+curr) % list.len()]);
+			}
+			vec.reverse();
+			let len = list.len();
+			for i in 0..length {
+				list[(i + curr) % len] = vec[i];
+			}
+			curr += length + skip_size;
+			skip_size += 1;
+		}
+	}
+
+	let part2 = list.chunks(16).map(|x| x.iter().fold(0, |acc, x| acc ^ x)).map(|x| format!("{:x}", x)).collect::<String>();
+	println!("Day 10 part 2: {}", part2);
 }
