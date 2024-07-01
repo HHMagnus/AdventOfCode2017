be51784217b514f33c179fe9579ac9fa8bd6bb04
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-07-01 21:10:15 +0200

    day 16 part 2

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,10 +1,36 @@
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 
 fn main() {
 	let file = read_to_string("input/day16.txt").unwrap();
 
 	let mut vec = (0..16).map(|x| char::from_u32('a' as u32 + x).unwrap()).collect::<Vec<_>>();
 
+	dance(&mut vec, &file);
+
+	let part1 = vec.into_iter().collect::<String>();
+	println!("Day 16 part 1: {}", part1);
+
+	let mut vec = (0..16).map(|x| char::from_u32('a' as u32 + x).unwrap()).collect::<Vec<_>>();
+
+	let mut states = HashMap::new();
+
+	let mut i = 0;
+	while i < 1000000000 {
+		dance(&mut vec, &file);
+		if states.contains_key(&vec) {
+			let &other = states.get(&vec).unwrap();
+			let diff = i - other;
+			i += ((1000000000 - i) / diff) * diff;
+		}
+		states.insert(vec.clone(), i);
+		i += 1;
+	}
+	
+	let part2 = vec.into_iter().collect::<String>();
+	println!("Day 16 part 2: {}", part2);
+}
+
+fn dance(vec: &mut Vec<char>, file: &str) {
 	for line in file.split(",") {
 		if line.starts_with("s") {
 			let xs = line.replace("s", "").parse::<usize>().unwrap();
@@ -15,7 +41,7 @@ fn main() {
 			for i in 0..vec.len()-xs {
 				new.push(vec[i]);
 			}
-			vec = new;
+			*vec = new;
 		}
 		if line.starts_with("x") {
 			let xs = line[1..].split("/").map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
@@ -32,7 +58,4 @@ fn main() {
 			vec[b] = temp;
 		}
 	}
-
-	let part1 = vec.into_iter().collect::<String>();
-	println!("Day 16 part 1: {}", part1);
 }
